7b811e61c50838800f0fc6138d343635d697f221
Bhrugu <bhrugupadhi@gmail.com>
2022-11-10 01:47:01 +0530

    day-2

--- myntra-frontend/src/components/AllRoutes.jsx
+++ myntra-frontend/src/components/AllRoutes.jsx
@@ -1,9 +1,16 @@
 import React from 'react'
+import { Route, Routes } from 'react-router-dom'
+import Singlepage from '../pages/Singlepage'
+import Home from './Home'
 
 const AllRoutes = () => {
   return (
-    <div>AllRoutes</div>
+    <div>
+      <Routes>
+        <Route path='/' element={<Home/>}></Route>
+        <Route path='/singlepage' element={<Singlepage/>}></Route>
+      </Routes>
+    </div>
   )
 }
-
 export default AllRoutes
--- myntra-frontend/src/components/Home.jsx
+++ myntra-frontend/src/components/Home.jsx
@@ -6,4 +6,4 @@ const Home = () => {
   )
 }
 
-export default Home
+export default Home;

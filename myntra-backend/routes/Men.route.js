337f83adb286d5b9364b1e862e352916b8555c8d
HARISH RAYALA <100848582+HarishRayala@users.noreply.github.com>
2022-11-13 12:22:55 +0530

    query

--- myntra-backend/routes/Men.route.js
+++ myntra-backend/routes/Men.route.js
@@ -6,6 +6,8 @@ menRouter.use(express.json());
 
 menRouter.get("/mensdata",MensDataController.getMenData);
 
+menRouter.get("/menspage",MensDataController.filterdata);
+
 menRouter.get("/mensdata/:brand",MensDataController.getBrand);
 
 menRouter.post("/create",MensDataController.postMensData);
